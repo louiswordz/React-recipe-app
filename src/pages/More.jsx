5b71710829d1fb180b5f6d74d8ac5dd14a67dc2d
louiswordz <85586219+louiswordz@users.noreply.github.com>
2025-01-02 09:05:44 +0000

    styled the more section

--- src/pages/More.jsx
+++ src/pages/More.jsx
@@ -7,12 +7,48 @@ const More = () => {
         <h2>Add Recipe</h2>
       </div>
       <div className="recipe-form">
-        <form>
-          <input type="text" name="meal" id="meal" />
-          <input type="file" name="meal" id="meal" />
-          <input type="text" name="meal" id="meal" />
-          <input type="text" name="meal" id="meal" />
-          <input type="submit" value='add more' id="meal" />
+        <form className="form-element">
+          <input
+            type="text"
+            name="meal"
+            id="meals"
+            className="meal"
+            placeholder="Enter the Recipe"
+          />
+          <input
+            type="url"
+            name="img-url"
+            id="img-url"
+            className="meal"
+            placeholder="Enter the image url"
+          />
+          <input
+            type="text"
+            name="Ingredient"
+            id="Ingredient"
+            className="meal"
+            placeholder="Enter the Ingredient"
+          />
+          <input
+            type="text"
+            name="size"
+            id="size"
+            className="meal"
+            placeholder="Enter the size"
+          />
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="meal"
+            placeholder="Enter the price"
+          />
+          <textarea
+            className="meal"
+            placeholder="Enter the recipe description"
+          ></textarea>
+
+          <input type="submit" value="add more" id="submit" className="meal" />
         </form>
       </div>
     </main>
